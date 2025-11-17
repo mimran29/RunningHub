920bbcea78d850b25fc672cf0dbdca4681333807
Imran <mimrancyrinus@gmail.com>
2025-11-18 01:23:09 +0800

    implement api routes,server actions and caching and event details page

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,37 @@
-import React from 'react';
 import ExploreBtn from "@/components/ExploreBtn";
 import EventCard from "@/components/EventCard";
-import {events} from "@/lib/constants";
+import {IEvent} from "@/database";
+import {cacheLife} from "next/cache";
 
-const Page = () => {
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+const Page = async () => {
+    'use cache';
+    cacheLife('hours')
+
+    const response = await fetch(`${BASE_URL}/api/events`);
+    const { events } = await response.json();
+
     return (
         <section>
+            <h1 className="text-center">The Hub for Every Dev <br /> Event You Can't Miss</h1>
+            <p className="text-center mt-5">Hackathons, Meetups, and Conferences, All in One Place</p>
 
-            <h1 className="text-center mt-10">
-                The Hub for Every Running Event<br/> You Can't Miss
-            </h1>
+            <ExploreBtn />
 
-            <p className="text-center mt-5">Road Marathon, Trail Running, and Fun Run, All in one Place !</p>
-            <ExploreBtn/>
-
-            <div className ="mt-20 space-y-7">
-                <h3> Featured Events </h3>
+            <div className="mt-20 space-y-7">
+                <h3>Featured Events</h3>
 
                 <ul className="events">
-                    {events.map((event)=>(
-                        <li key={event.title}>
+                    {events && events.length > 0 && events.map((event: IEvent) => (
+                        <li key={event.title} className="list-none">
                             <EventCard {...event} />
                         </li>
                     ))}
-
                 </ul>
             </div>
-
-
         </section>
-
-    );
-};
+    )
+}
 
 export default Page;
