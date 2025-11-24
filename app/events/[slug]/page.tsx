7251a0c8561366c73d87828c12f4726fd253bec9
Imran <mimrancyrinus@gmail.com>
2025-11-24 14:12:19 +0800

    finalized

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -4,6 +4,7 @@ import BookEvent from "@/components/BookEvent";
 import {IEvent} from "@/database";
 import {getSimilarEventsBySlug} from "@/lib/actions/event.actions";
 import EventCard from "@/components/EventCard";
+import {cacheLife} from "next/cache";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
@@ -42,6 +43,8 @@ const EventTags = ({ tags }: { tags: string[] }) => (
 
 
 const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }>}) => {
+    'use cache'
+    cacheLife('hours');
     const { slug } = await params;
 
     let event;
@@ -125,7 +128,7 @@ const EventDetailsPage = async ({ params }: { params: Promise<{ slug: string }>}
                             <p className="text-sm">Be the first to book your spot!</p>
                         )}
 
-                        <BookEvent />
+                        <BookEvent eventId={event._id} slug={slug}/>
                     </div>
                 </aside>
             </div>
