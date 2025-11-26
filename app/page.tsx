814395365b79d14b90e26b2a603301a39d4f85e1
Imran <mimrancyrinus@gmail.com>
2025-11-26 21:33:21 +0800

    real2?

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ const Page = async () => {
     'use cache';
     cacheLife('hours')
 
-   /const response = await fetch(`${BASE_URL}/api/events`);
+   const response = await fetch(`${BASE_URL}/api/events`);
    const { events } = await response.json();
 
     return (
